135b6745a314e8f942307b04da11a0af99007365
Teemu Matilainen <teemu.matilainen@reaktor.fi>
2012-10-27 22:22:22 -0300

    Add `solo bootstrap` command
    Implement more or less the same funkctionality as `knife bootstrap` but
    using chef-solo. Calls `prepare` and `cook` commands to install Chef and
    to run chef-solo on the node.

--- lib/chef/knife/cook.rb
+++ lib/chef/knife/cook.rb
@@ -10,8 +10,8 @@ class Chef
     # Approach ported from spatula (https://github.com/trotter/spatula)
     # Copyright 2009, Trotter Cashion
     class Cook < Knife
-      OMNIBUS_EMBEDDED_PATHS  = ["/opt/chef/embedded/bin", "/opt/opscode/embedded/bin"]
-      CHEF_VERSION_CONSTRAINT = ">=0.10.4"
+      OMNIBUS_EMBEDDED_PATHS  ||= ["/opt/chef/embedded/bin", "/opt/opscode/embedded/bin"]
+      CHEF_VERSION_CONSTRAINT ||= ">=0.10.4"
 
       include KnifeSolo::SshCommand
       include KnifeSolo::KitchenCommand
